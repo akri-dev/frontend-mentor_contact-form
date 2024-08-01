ff98df2e5ae3054ed194059df0f1f98eaa4fcefb
akri <alecjoseph.dev@gmail.com>
2024-08-01 14:04:46 +0800

    working on synchronous or dynamic form validation

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,22 @@
+// format is the same in creating IPO Structure
+const form = document.forms["contact_form"];
+const validationParagraph = document.getElementsByClassName("validation-paragraph");
+const validationQuery = document.getElementById("validation-query-type");
+
+const fieldsToValidate = ["first_name", "last_name", "email_address", "message_box"];
+const emailAddressToValidate = ["email_address"];
+const checkboxToValidate = ["consent_checkbox"];
+const validationQueryRadio = document.getElementsByName("query_type");
+const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // for validating email, use test()
+
+
+let isFormValid = true; // Tracks overall field form validity
+let isEmailFieldEmpty = true; // tracks if Email Field is Empty  (for validation if Email is right 
+let isQueryRadioSelected = false;
+
+// validate empty query
+var validationQueryCounter = 0
+
 function successMessage() {
 }
 
@@ -14,19 +33,6 @@ function validateForm() {
     //       } // loop when need to work on multiple arrays with the same data value   
     // }
 
-    // format is the same in creating IPO Structure
-    const form = document.forms["contact_form"];
-    const validationParagraph = document.getElementsByClassName("validation-paragraph");
-    const validationQuery = document.getElementById("validation-query-type");
-
-    const fieldsToValidate = ["first_name", "last_name", "email_address", "message_box"];
-    const emailAddressToValidate = ["email_address"];
-    const validationQueryRadio = document.getElementsByName("query_type");
-
-    let isFormValid = true; // Tracks overall field form validity
-    let isEmailFieldEmpty = true; // tracks if Email Field is Empty  (for validation if Email is right 
-    let isQueryRadioSelected = false;
-
     //validates 4 field empty
     for (let i = 0; i < fieldsToValidate.length; i++) { 
         const fieldName = fieldsToValidate[i];
@@ -39,9 +45,6 @@ function validateForm() {
             validationParagraph[i].innerHTML = "";
         }
     }
-
-    // validate empty query
-    var validationQueryCounter = 0
 
     //while tracker is false AND Query Counter is 0 less than 2, then it's true
     while (!isQueryRadioSelected && validationQueryCounter < validationQueryRadio.length) {
@@ -65,10 +68,16 @@ function validateForm() {
     }
     
     //for arrays, include form element
-    if (!form[emailAddressToValidate].value.includes('@')) { //if email address input !include @
+    if (!emailRegEx.test(form[emailAddressToValidate].value)) { //if email address input !include @
         validationParagraph[2].innerHTML = "Please enter a valid email address"; 
     } else {
         if(!isEmailFieldEmpty) validationParagraph[2].innerHTML = "";
+    }
+
+    if(!form[checkboxToValidate].checked) {
+        validationParagraph[4].innerHTML = " To submit this form, please consent to being contacted";
+    } else {
+        validationParagraph[4].innerHTML = "";
     }
 
     if (!isFormValid && !isQueryRadioSelected) { // if tracker is false, return value will be false
@@ -76,18 +85,37 @@ function validateForm() {
 
     } else {
         return true;
-    }
-    
+    }   
 }
 
-function uncheckRadio() {
-    const radioButtons = document.getElementsByName("query_type");
+// function uncheckRadio() {
+//     const radioButtons = document.getElementsByName("query_type");
 
-    for (var i = 0 ; i < radioButtons.length ; i++) {
-        if (radioButtons[i].checked == true) {
-            console.log(radioButtons[i].checked);
-            radioButtons[i].checked = false;
-            console.log(radioButtons[i].checked);
-        }
+//     for (var i = 0 ; i < radioButtons.length ; i++) {
+//         if (radioButtons[i].checked == true) {
+//             console.log(radioButtons[i].checked);
+//             radioButtons[i].checked = false;
+//             console.log(radioButtons[i].checked);
+//         }
+//     }
+// }
+
+function checkBox() {
+    const checkBox = document.getElementById("consent-checkbox");
+
+    if (!checkBox.checked === true) {
+        validationParagraph[4].innerHTML = "To submit this form, please consent to being contacted";
+    } else {
+        validationParagraph[4].innerHTML = "";
     }
 }
+
+function fieldSynchronousValidation(arrayValue) {
+    const fieldValue = form[fieldsToValidate[arrayValue]].value.trim();
+    if (fieldValue === "") {
+        validationParagraph[arrayValue].innerHTML = "This field is required";
+    } else {
+        validationParagraph[arrayValue].innerHTML = "";
+    }
+}
+
